0738db4bb5d202b835b6343c2ffc753329dd62c6
naturecool <dream0fly@aliyun.com>
2016-08-25 15:31:33 +0800

    Update huancheng.js

--- hcvr/js/huancheng.js
+++ hcvr/js/huancheng.js
@@ -517,13 +517,13 @@ $('#begin').on('click', function () {
 		krpano.call("webvr.enterVR()");
 		
 		
-		$('body').on('click', function () {
+/*		$('body').on('click', function () {
 			
 			krpano.call('vr_auto_click(get(vr_timeout))');
 			
 			console.log('vr_auto_click');
 			
-		});
+		});*/
 	
 		
 	}
