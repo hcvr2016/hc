ecd22b43d4bcc0c4b3bdbd7575b4d2fc534679d8
hcvr2016 <hcvr2016@163.com>
2016-08-27 00:10:36 +0800

    Update huancheng.js

--- hcvr/js/huancheng.js
+++ hcvr/js/huancheng.js
@@ -8,7 +8,7 @@ var krpano, constantScreenWidth, constantScreenHeight, resizeTimer, curSce,
 
 //$('.chat').hide();
 
-$(document).on("touchmove", function(evt) {
+$(document).on("touchmove", function(evt) {t
 
 	evt.preventDefault();
 
@@ -941,7 +941,7 @@ function showTxt (ifTriggerInvite) {
 }
 
 
-embedpano({swf:"http://hcvr2.github.io/hc/hcvr/tour.swf", xml:"https://hcvr2.github.io/hc/hcvr/tour.xml", target:"pano", html5:"only+webgl", initvars:{design:"flat"}, passQueryParameters:true, onready:krpanoReady});
+embedpano({swf:"http://hcvr2.github.io/hc/hcvr/tour.swf", xml:"https://hcvr2016.github.io/hc/hcvr/tour.xml", target:"pano", html5:"only+webgl", initvars:{design:"flat"}, passQueryParameters:true, onready:krpanoReady});
  
 
 function krpanoReady(krpanObj)
